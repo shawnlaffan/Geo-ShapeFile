03343e47ba717ee2a48ac3d6600c464b6605303e
Shawn Laffan <shawnlaffan@gmail.com>
2014-02-16 20:43:05 +1100

    Update the POD. Also a few variable name changes to fix on a stable nomenclature.
    Signed-off-by: Shawn Laffan <shawnlaffan@gmail.com>

--- lib/Geo/ShapeFile/Shape/Index.pm
+++ lib/Geo/ShapeFile/Shape/Index.pm
@@ -5,6 +5,8 @@ use POSIX qw /floor/;
 use Carp;
 use autovivification;
 
+our $VERSION = '2.55_001';
+
 #  should also handle X cells
 sub new {
     my ($class, $n, $x_min, $y_min, $x_max, $y_max) = @_;
@@ -13,7 +15,7 @@ sub new {
 
     $n ||= 10;  #  need a better default?
     $n   = int $n;
-    die "Number of boxes must be positive and >=1"
+    die 'Number of blocks must be positive and >=1'
       if $n <= 0;
 
     my $y_range = abs ($y_max - $y_min);
@@ -21,24 +23,25 @@ sub new {
     $y_range   += 2 * $y_tol;
     $y_min     -= $y_tol;
     $y_max     += $y_tol;
-    my $box_ht  = $y_range / $n;
+
+    my $block_ht = $y_range / $n;
 
     $self->{x_min} = $x_min;
     $self->{y_min} = $y_min;
     $self->{x_max} = $x_max;
     $self->{y_max} = $y_max;
-    $self->{y_res} = $box_ht;
+    $self->{y_res} = $block_ht;
     $self->{y_n}   = $n;
     $self->{x_n}   = 1;
 
-    my %boxes;
+    my %blocks;
     my $y = $y_min;
     foreach my $i (1 .. $n) {
         my $key = $self->snap_to_index($x_min, $y);  #  index by lower left
-        $boxes{$key} = [];
-        $y += $box_ht;
+        $blocks{$key} = [];
+        $y += $block_ht;
     }
-    $self->{containers} = \%boxes;
+    $self->{containers} = \%blocks;
 
     return $self;
 }
@@ -61,6 +64,7 @@ sub _get_container_ref {
     return $container;
 };
 
+#  need to handle X coords as well
 sub snap_to_index {
     my ($self, $x, $y) = @_;
 
@@ -68,17 +72,17 @@ sub snap_to_index {
     my $y_min = $self->get_y_min;
     my $y_res = $self->get_y_res;
 
-    my $box_y = floor (($y - $y_min) / $y_res);
+    my $y_block = floor (($y - $y_min) / $y_res);
 
-    return wantarray ? (0, $box_y) : "0:$box_y";
+    return wantarray ? (0, $y_block) : "0:$y_block";
 }
 
-#  inserts into whichever boxes overlap the bounding box
+#  inserts into whichever blocks overlap the bounding box
 sub insert {
     my ($self, $item, @bbox) = @_;
 
-    my @index_id1  = $self->snap_to_index (@bbox[0, 1]);
-    my @index_id2  = $self->snap_to_index (@bbox[2, 3]);
+    my @index_id1 = $self->snap_to_index (@bbox[0, 1]);
+    my @index_id2 = $self->snap_to_index (@bbox[2, 3]);
 
     my $insert_count = 0;
     foreach my $y ($index_id1[1] .. $index_id2[1]) {
@@ -91,13 +95,13 @@ sub insert {
     return $insert_count;
 }
 
-#  $storage ref is for Tree::R compat
+#  $storage ref arg is for Tree::R compat - still needed?
 sub query_point {
     my ($self, $x, $y, $storage_ref) = @_;
 
     my $index_id  = $self->snap_to_index ($x, $y);
     my $container = $self->_get_container_ref ($index_id);
-    
+
     if ($storage_ref) {
         push @$storage_ref, @$container;
     }
@@ -119,7 +123,7 @@ Geo::ShapeFile::Shape - Geo::ShapeFile utility class.
 
   my $index = Geo::ShapeFile::Shape->new;
   #  $pt1 and $pt2 are point objects in this example.  
-  my $segment = [$pt1, $pt2];  #  example of something to pack.
+  my $segment = [$pt1, $pt2];  #  example of something to pack into the index.
   my @bbox = ($x_min, $y_min, $x_max, $y_max);
   $index->insert($segment, @bbox);
 
@@ -130,12 +134,23 @@ Geo::ShapeFile::Shape - Geo::ShapeFile utility class.
 
 =head1 DESCRIPTION
 
-This is an index class for Geo::ShapeFile::Shape objects.
-It probably has more generic applications, of course.  
-It uses a flat 2-d structure comprising a series of boxes of full width
-which slice the shape along the y-axis (should really use boxes to leverage the x axis).
+This is a 2-d block-based index class for Geo::ShapeFile::Shape objects.
+It probably has more generic applications, of course.
+
+It uses a flat 2-d structure comprising a series of blocks of full width
+which slice the shape along the y-axis (it should really also use blocks
+along the x axis).
+
+The index coordinates are simply the number of blocks across and up
+from the minimum coordinate specified in the new() call.  These are stoed as
+strings jpoined by a colon, so 0:0 is the lower left.
+Negative block coordinates can occur if data are added which fall outside the
+speficied bounds.  This should not affect the index, though, as it is merely
+a relative offset.
+
 It is used internally by Geo::ShapeFile::Shape, so look there for examples.  
-The method names are adapted from Tree::R to make ytransition easier during development.  
+The method names are adapted from Tree::R to make transition easier during development,
+albeit the argument have morphed so it is not a drop-in replacement. 
 
 
 =head2 EXPORT
@@ -146,17 +161,39 @@ None by default.
 
 =over 4
 
-=item new($n_boxes, @bbox)
+=item new($n_blocks_y, @bbox)
 
 Creates a new Geo::ShapeFile::Shape::Index objectand returns it.
 
-$n_boxes is the number of boxes along the y-axis.
+$n_blocks_y is the number of blocks along the y-axis.
 @bbox is the bounding box the index represents (x_min, y_min, x_max, y_max).
 
-=item insert($item, $x, $y)
+=item insert($item, $min_x, $min_y, $max_x, $max_y)
 
-Adds item $item to the box which contains point $x, $y.
+Adds item $item to the blocks which overlap with the specified bounds.
+Returns the number of blocks the item was added to.
 
+=item query_point($x, $y)
+
+Returns an array of objects on the block contains point $x,$y.
+Returns an arrayref in scalar context.
+
+
+=item get_x_max() get_x_min() get_y_max() get_y_min()
+
+Bounds of the index, as set in the call to ->new().
+There is no guarantee they are the bounds of the data, as
+data outside the original bounds can be indexed.
+
+=item get_y_res()
+
+Block resolution along the y-axis.
+
+=item snap_to_index ($x, $y)
+
+Returns the index key associated with point $x,$y.
+Does not check if it is outside the bounds of the index,
+so negative index values are possible.
 
 
 =back
